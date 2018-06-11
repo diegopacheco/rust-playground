e393124668d0a5985cc0b56c91e20dbfd325ee4f
Diego Pacheco <diego.pacheco.it@gmail.com>
2018-06-10 18:58:14 -0700

    complete guess game

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -11,7 +11,11 @@ fn main() {
         println!("Please input your guess.");
         let mut guess = String::new();
         io::stdin().read_line(&mut guess).expect("Failed to read line");
-        let guess: u32 = guess.trim().parse().expect("Please type a number!");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
 
         println!("You guessed: {}", guess);
         match guess.cmp(&secret_number) {
