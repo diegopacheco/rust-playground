530edff452217d7139dfb3347e1ccd38276b630d
Diego Pacheco <diego.pacheco.it@gmail.com>
2018-06-10 19:29:21 -0700

    functions

---
+++ basic_lang/src/functions.rs
@@ -0,0 +1,3 @@
+pub fn addOne(i : i32) -> i32{
+    i + 1
+}
--- basic_lang/src/main.rs
+++ basic_lang/src/main.rs
@@ -1,6 +1,13 @@
 mod vars;
+mod functions;
 
 fn main() {
     println!("Hello, world!");
     vars::execute();
+
+    let i = functions::addOne(10);
+    println!("addOne: {:?}",i);
+    let f:fn(i32)->i32 = functions::addOne;
+    println!("addOne: {:?}",f(i));
+    
 }
