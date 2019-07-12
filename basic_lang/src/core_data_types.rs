6632ca563065667ba6f076e86ffc01b678617119
Diego Pacheco <diego.pacheco.it@gmail.com>
2019-07-11 21:43:33 -0300

    mutable

--- basic_lang/src/core_data_types.rs
+++ basic_lang/src/core_data_types.rs
@@ -1,4 +1,12 @@
 pub fn execute(){
-    let a:u8 = 255; // 8 bits u => unsigned(0 or positive number), value can be 0..255
-    print!("{}",a);
+    // is also immutable variable
+    // 8 bits u => unsigned(0 or positive number), value can be 0..255
+    let a:u8 = 255; 
+    print!("a = {}",a);
+
+    // b is mutable because we use the keyword mut
+    let mut b:i8 = 0;
+    print!(" b = {}",b);
+    b=42;
+    print!(" b = {}",b);
 }
