b59acf155929e79ec10a98f4266dd14826b44501
Diego Pacheco <diego.pacheco.it@gmail.com>
2019-10-29 01:05:22 -0700

    fun with actix

---
+++ hello-world-actix/src/main.rs
@@ -0,0 +1,21 @@
+use actix_web::{web, App, HttpResponse, HttpServer, Responder};
+
+fn index() -> impl Responder {
+    HttpResponse::Ok().body("Hello world!")
+}
+
+fn index2() -> impl Responder {
+    HttpResponse::Ok().body("Hello world again!")
+}
+
+fn main() {
+    HttpServer::new(|| {
+        App::new()
+            .route("/", web::get().to(index))
+            .route("/again", web::get().to(index2))
+    })
+    .bind("127.0.0.1:8088")
+    .unwrap()
+    .run()
+    .unwrap();
+}
