c2de1a5080801e6c1b4b212779f9e60766a0adb2
Diego Pacheco <diego.pacheco.it@gmail.com>
2019-11-01 11:55:04 -0700

    fun with nickel

---
+++ nickel-fun/src/main.rs
@@ -0,0 +1,15 @@
+#[macro_use] extern crate nickel;
+use nickel::Nickel;
+
+fn main() {
+    let mut server = Nickel::new();
+
+    server.utilize(router! {
+        get "**" => |_req, _res| {
+            "Hello world!"
+        }
+    });
+
+    let r = server.listen("127.0.0.1:6767");
+    println!("{:?}",r);
+}
