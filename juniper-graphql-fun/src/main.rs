2b2b404f9d7755e2beeecb6db8d087530bc04287
Diego Pacheco <diego.pacheco.it@gmail.com>
2019-12-12 21:11:55 -0300

    rust + graphql

---
+++ juniper-graphql-fun/src/main.rs
@@ -0,0 +1,55 @@
+use std::io;
+use std::sync::Arc;
+
+#[macro_use]
+extern crate juniper;
+
+use actix_web::{middleware, web, App, Error, HttpResponse, HttpServer};
+use juniper::http::graphiql::graphiql_source;
+use juniper::http::GraphQLRequest;
+
+mod schema;
+
+use crate::schema::{create_schema, Schema};
+
+async fn graphiql() -> HttpResponse {
+    let html = graphiql_source("http://127.0.0.1:8080/graphql");
+    HttpResponse::Ok()
+        .content_type("text/html; charset=utf-8")
+        .body(html)
+}
+
+async fn graphql(
+    st: web::Data<Arc<Schema>>,
+    data: web::Json<GraphQLRequest>,
+) -> Result<HttpResponse, Error> {
+    let user = web::block(move || {
+        let res = data.execute(&st, &());
+        Ok::<_, serde_json::error::Error>(serde_json::to_string(&res)?)
+    })
+    .await?;
+    Ok(HttpResponse::Ok()
+        .content_type("application/json")
+        .body(user))
+}
+
+#[actix_rt::main]
+async fn main() -> io::Result<()> {
+    std::env::set_var("RUST_LOG", "actix_web=info");
+    env_logger::init();
+
+    // Create Juniper schema
+    let schema = std::sync::Arc::new(create_schema());
+
+    // Start http server
+    HttpServer::new(move || {
+        App::new()
+            .data(schema.clone())
+            .wrap(middleware::Logger::default())
+            .service(web::resource("/graphql").route(web::post().to(graphql)))
+            .service(web::resource("/graphiql").route(web::get().to(graphiql)))
+    })
+    .bind("127.0.0.1:8080")?
+    .start()
+    .await
+}
