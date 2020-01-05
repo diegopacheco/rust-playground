bb392729a509a7e11767fe563e96e2ee342f92ec
Diego Pacheco <diego.pacheco.it@gmail.com>
2020-01-05 00:13:26 -0300

    cursive fun

---
+++ cursive-fun/src/main.rs
@@ -0,0 +1,16 @@
+extern crate cursive;
+use cursive::Cursive;
+use cursive::views::{Dialog, TextView};
+
+fn main() {
+    // Creates the cursive root - required for every application.
+    let mut siv = Cursive::default();
+
+    // Creates a dialog with a single "Quit" button
+    siv.add_layer(Dialog::around(TextView::new("Hello Dialog!"))
+                         .title("Cursive")
+                         .button("Quit", |s| s.quit()));
+
+    // Starts the event loop.
+    siv.run();
+}
