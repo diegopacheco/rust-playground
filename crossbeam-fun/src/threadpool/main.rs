bfd55546b6b8851b4fe8edde1644babe404ab171
Diego Pacheco <diego.pacheco.it@gmail.com>
2020-01-06 13:06:20 -0300

    fun with crossbean with TP

--- crossbeam-fun/src/threadpool/main.rs
+++ crossbeam-fun/src/threadpool/main.rs
@@ -3,17 +3,13 @@ extern crate num;
 extern crate num_cpus;
 extern crate image;
 
-use std::sync::mpsc::{channel, RecvError};
+use std::sync::mpsc::{channel};
 use threadpool::ThreadPool;
-use num::complex::Complex;
-use image::{ImageBuffer, Pixel, Rgb};
+use image::{ImageBuffer};
 
-fn main() -> Result<()> {
+fn main() -> Result<(),()> {
     let (width, height) = (1920, 1080);
     let mut img = ImageBuffer::new(width, height);
-    let iterations = 300;
-
-    let c = Complex::new(-0.8, 0.156);
 
     let pool = ThreadPool::new(num_cpus::get());
     let (tx, rx) = channel();
@@ -21,16 +17,16 @@ fn main() -> Result<()> {
     for y in 0..height {
         let tx = tx.clone();
         pool.execute(move || for x in 0..width {
-                         let i = julia(c, x, y, width, height, iterations);
-                         let pixel = wavelength_to_rgb(380 + i * 400 / iterations);
+                         let i = 1;
+                         let pixel = image::Rgb([i, 1, 1]);
                          tx.send((x, y, pixel)).expect("Could not send data!");
                      });
     }
 
     for _ in 0..(width * height) {
-        let (x, y, pixel) = rx.recv()?;
+        let (x, y, pixel) = rx.recv().unwrap();
         img.put_pixel(x, y, pixel);
     }
-    let _ = img.save("output.png")?;
+    let _ = img.save("output.png");
     Ok(())
 }
