f998dbb1b268ad5985dfffdf54f4096f62a62989
Diego Pacheco <diego.pacheco.it@gmail.com>
2020-01-12 01:39:09 -0300

    migrations working

--- rust-microservice/news-migrations/src/main.rs
+++ rust-microservice/news-migrations/src/main.rs
@@ -1,14 +1,16 @@
 extern crate barrel;
 
-use barrel::*;
+use barrel::migration::Migration;
+use barrel::types;
+use barrel::backend::Pg;
 
 fn main() {
-    let mut m = Migration::new();
 
+    let mut m = Migration::new();
     m.create_table("news", |t| {
         t.add_column("id", types::primary());
         t.add_column("desc", types::varchar(255));
         t.add_column("url", types::varchar(255));
     });
-    println!("{}", m.schema.unwrap());
+    println!("{:?}", m.make::<Pg>());
 }
