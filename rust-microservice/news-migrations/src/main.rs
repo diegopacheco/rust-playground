f6f9cad907b862e8283bf502375e2ca808f0a6d5
Diego Pacheco <diego.pacheco.it@gmail.com>
2020-01-12 13:07:43 -0300

    migration working

--- rust-microservice/news-migrations/src/main.rs
+++ rust-microservice/news-migrations/src/main.rs
@@ -1,16 +1,29 @@
 extern crate barrel;
+extern crate postgres;
 
+use postgres::Client;
+use postgres::tls::NoTls;
 use barrel::migration::Migration;
 use barrel::types;
 use barrel::backend::Pg;
 
-fn main() {
+fn main() -> Result<(), postgres::Error> {
 
     let mut m = Migration::new();
     m.create_table("news", |t| {
-        t.add_column("id", types::primary());
-        t.add_column("desc", types::varchar(255));
-        t.add_column("url", types::varchar(255));
+        t.add_column("id", types::uuid().primary(true));
+        t.add_column("desc", types::text());
+        t.add_column("url", types::text());
     });
-    println!("{}", m.make::<Pg>());
+    let news_table = m.make::<Pg>().to_owned();
+    println!("Table {} will be created",news_table);
+    
+    let mut conn = Client::connect("postgres://postgres:docker@172.17.0.2:5432/postgres", NoTls).unwrap();
+    let result = conn.execute(&news_table[..],&[]);
+    match result{
+        Ok(changes) => print!("Migrations done. Changes Done {}",changes),
+        Err(error)  => print!("Error: {}",error),
+    };
+    
+    Ok(())
 }
