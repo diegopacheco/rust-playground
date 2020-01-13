148ab76f53640be5722a2955b0fede1a0817b2c3
Diego Pacheco <diego.pacheco.it@gmail.com>
2020-01-13 00:52:57 -0300

    refactoring - 100% working

--- rust-microservice/news-dao/src/lib.rs
+++ rust-microservice/news-dao/src/lib.rs
@@ -6,7 +6,7 @@ use news_contract::News;
 use tokio_postgres::{NoTls};
 use tokio;
 
-pub async fn insert_news(url:&String,desc:&String) -> Option<News> {
+pub async fn connect() -> Option<tokio_postgres::Client> {
   let (client,conn) =
         tokio_postgres::connect("host=172.17.0.2 user=postgres password=docker dbname=postgres port=5432", NoTls).
         await.unwrap();
@@ -15,7 +15,11 @@ pub async fn insert_news(url:&String,desc:&String) -> Option<News> {
           eprintln!("connection error: {}", e);
       }
   });
-  
+  return Some(client);
+}
+
+pub async fn insert_news(url:&String,desc:&String) -> Option<News> {
+  let client = connect().await.unwrap();
   let _row = client.query("INSERT INTO news VALUES(uuid_in(md5(random()::text || clock_timestamp()::text)::cstring),$1,$2)",&[&desc,&url]).await.unwrap();
   let news = News {
     id: String::from("0"),
@@ -26,16 +30,7 @@ pub async fn insert_news(url:&String,desc:&String) -> Option<News> {
 }
 
 pub async fn list_news() -> Option<Vec<News>> {
-
-  let (client,conn) =
-        tokio_postgres::connect("host=172.17.0.2 user=postgres password=docker dbname=postgres port=5432", NoTls).
-        await.unwrap();
-  tokio::spawn(async move {
-      if let Err(e) = conn.await {
-          eprintln!("connection error: {}", e);
-      }
-  });      
-
+  let client = connect().await.unwrap();
   let mut vec_news = Vec::new();  
   let rows = &client.query("SELECT id::text,url,'desc' FROM news", &[]).await.unwrap();
   for row in rows {
