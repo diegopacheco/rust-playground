ffe4f11e5bc1973a91ab2dc9e4c2f7496228cbe2
Diego Pacheco <diego.pacheco.it@gmail.com>
2020-01-14 23:40:44 -0300

    fun with lazy static

---
+++ lazy_static_fun/src/main.rs
@@ -0,0 +1,23 @@
+#[macro_use] extern crate lazy_static;
+
+use std::collections::HashMap;
+
+lazy_static! {
+    static ref HASHMAP: HashMap<u32, &'static str> = {
+        let mut m = HashMap::new();
+        m.insert(0, "Gravatai");
+        m.insert(1, "Poa");
+        m.insert(2, "Gramado");
+        m
+    };
+}
+
+fn main() {
+
+    // First access to `HASHMAP` initializes it
+    println!("The entry for `0` is \"{}\".", HASHMAP.get(&0).unwrap());
+
+    // Any further access to `HASHMAP` just returns the computed value
+    println!("The entry for `1` is \"{}\".", HASHMAP.get(&1).unwrap());
+
+}
