af05c54fd1e4dc622eae3ac699bc07d403d7039b
Diego Pacheco <diego.pacheco.it@gmail.com>
2020-01-26 15:16:33 -0300

    streams fun

--- streams-fun/src/main.rs
+++ streams-fun/src/main.rs
@@ -3,6 +3,7 @@ extern crate async_std;
 use async_std::task;
 use async_std::prelude::*;
 use async_std::stream;
+use std::collections::HashMap;
 
 fn main() {
     task::block_on(run())
@@ -17,7 +18,18 @@ async fn run() {
         }
     }
 
-    let _b = stream::repeat(1u8)
-        .for_each(|num| println!("B: {:?}", num))
-        .await;
+    let a = stream::repeat(1u8).take(10);
+    let b = stream::repeat(2u8).take(10);
+    let mut nums = a.merge(b);
+    while let Some(num) = nums.next().await {
+        println!("num: {}", num);
+    }
+
+    let a = stream::once(1u8);
+    let b = stream::once(0u8);
+    let s = a.zip(b);
+    let _map:HashMap<u8, u8> = s.collect().await;
+    assert_eq!(_map.get(&1), Some(&0u8));
+
+
 }
