e3c9c2f04bb791309841c64f6a84ef592c30cc6f
Diego Pacheco <diego.pacheco.it@gmail.com>
2020-01-27 15:29:35 -0300

    rocksdb + rust

--- rocksdb-fun/src/main.rs
+++ rocksdb-fun/src/main.rs
@@ -1,10 +1,15 @@
+extern crate rocksdb;
+
+use rocksdb::DB;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Hello, world! RocksDB + Rust");
+
     // NB: db is automatically closed at end of lifetime
     let db = DB::open_default("/tmp/rocksdb/").unwrap();
-    db.put(b"my key", b"my value");
+    db.put(b"my key", b"my value").unwrap();
     match db.get(b"my key") {
-      Ok(Some(value)) => println!("retrieved value {}", value.to_utf8().unwrap()),
+      Ok(Some(value)) => println!("retrieved value {}", value.get(0).unwrap()),
       Ok(None) => println!("value not found"),
       Err(e) => println!("operational problem encountered: {}", e),
    }
