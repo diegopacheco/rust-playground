d71225547cbb4ffd27c07d8c0a1cf46fe51b1ae5
Diego Pacheco <diego.pacheco.it@gmail.com>
2020-01-27 14:57:37 -0300

    rocksdb + rust

--- rocksdb-fun/src/main.rs
+++ rocksdb-fun/src/main.rs
@@ -1,3 +1,12 @@
 fn main() {
     println!("Hello, world!");
+    // NB: db is automatically closed at end of lifetime
+    let db = DB::open_default("/tmp/rocksdb/").unwrap();
+    db.put(b"my key", b"my value");
+    match db.get(b"my key") {
+      Ok(Some(value)) => println!("retrieved value {}", value.to_utf8().unwrap()),
+      Ok(None) => println!("value not found"),
+      Err(e) => println!("operational problem encountered: {}", e),
+   }
+   db.delete(b"my key").unwrap();
 }
