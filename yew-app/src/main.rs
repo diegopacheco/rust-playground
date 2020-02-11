f7cf2e96e3b7ca97656b3510e777c73ade0d6a93
Diego Pacheco <diego.pacheco.it@gmail.com>
2020-02-11 18:11:44 -0300

    yew fun

---
+++ yew-app/src/main.rs
@@ -0,0 +1,43 @@
+use yew::{html, Callback, ClickEvent, Component, ComponentLink, Html, ShouldRender};
+
+struct App {
+    clicked: bool,
+    onclick: Callback<ClickEvent>,
+}
+
+enum Msg {
+    Click,
+}
+
+impl Component for App {
+    type Message = Msg;
+    type Properties = ();
+
+    fn create(_: Self::Properties, link: ComponentLink<Self>) -> Self {
+        App {
+            clicked: false,
+            onclick: link.callback(|_| Msg::Click),
+        }
+    }
+
+    fn update(&mut self, msg: Self::Message) -> ShouldRender {
+        match msg {
+            Msg::Click => {
+                self.clicked = true;
+                true // Indicate that the Component should re-render
+            }
+        }
+    }
+
+    fn view(&self) -> Html {
+        let button_text = if self.clicked { "Clicked!" } else { "Click me!" };
+
+        html! {
+            <button onclick=&self.onclick>{ button_text }</button>
+        }
+    }
+}
+
+fn main() {
+    yew::start_app::<App>();
+}
