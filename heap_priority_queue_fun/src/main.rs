d656439382247f4e0734c6ea2e6efdd6d8dccae1
Diego Pacheco <diego.pacheco.it@gmail.com>
2020-02-19 14:45:20 -0300

    priority queue

---
+++ heap_priority_queue_fun/src/main.rs
@@ -0,0 +1,18 @@
+extern crate priority_queue;
+
+use priority_queue::PriorityQueue;
+
+fn main() {
+    let mut pq = PriorityQueue::new();
+
+    assert!(pq.is_empty());
+    pq.push("Apples", 5);
+    pq.push("Bananas", 8);
+    pq.push("Strawberries", 23);
+
+    assert_eq!(pq.peek(), Some((&"Strawberries", &23)));
+
+    for (item, _) in pq.into_sorted_iter() {
+        println!("{}", item);
+    }
+}
