2bcaad36982f33fafec20c34c264024d747d21e2
Diego Pacheco <diego.pacheco.it@gmail.com>
2020-02-19 15:02:37 -0300

    priority queue

--- heap_priority_queue_fun/src/main.rs
+++ heap_priority_queue_fun/src/main.rs
@@ -9,9 +9,10 @@ fn main() {
     pq.push("Apples", 5);
     pq.push("Bananas", 8);
     pq.push("Strawberries", 23);
-
     assert_eq!(pq.peek(), Some((&"Strawberries", &23)));
 
+    println!("Get from: {:?}",pq.get(&"Apples").unwrap());
+    println!("For each... ");
     for (item, _) in pq.into_sorted_iter() {
         println!("{}", item);
     }
