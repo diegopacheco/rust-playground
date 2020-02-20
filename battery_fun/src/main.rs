364db54d39908847f3c4e2662a29b0a3d59f76b2
Diego Pacheco <diego.pacheco.it@gmail.com>
2020-02-20 10:01:11 -0300

    battery

---
+++ battery_fun/src/main.rs
@@ -0,0 +1,15 @@
+fn main() -> Result<(), battery::Error> {
+    let manager = battery::Manager::new()?;
+
+    for (idx, maybe_battery) in manager.batteries()?.enumerate() {
+        let battery = maybe_battery?;
+        println!("Battery #{}:", idx);
+        println!("Vendor: {:?}", battery.vendor());
+        println!("Model: {:?}", battery.model());
+        println!("State: {:?}", battery.state());
+        println!("Time to full charge: {:?}", battery.time_to_full());
+        println!("");
+    }
+
+    Ok(())
+}
