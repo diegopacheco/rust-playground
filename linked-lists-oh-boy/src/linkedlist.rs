581a28a041be5436f28c78903c3736ca82253f2a
diegopacheco <diego.pacheco.it@gmail.com>
2020-02-24 22:37:39 -0300

    impl linked list with generics in Rust

--- linked-lists-oh-boy/src/linkedlist.rs
+++ linked-lists-oh-boy/src/linkedlist.rs
@@ -30,6 +30,18 @@ impl<T> List<T> {
             node.elem
         })
     }
+
+    pub fn peek(&self) -> Option<&T> {
+        self.head.as_ref().map(|node| {
+            &node.elem
+        })
+    }
+
+    pub fn peek_mut(&mut self) -> Option<&mut T> {
+        self.head.as_mut().map(|node| {
+            &mut node.elem
+        })
+    }
 }
 
 impl<T> Drop for List<T> {
@@ -39,4 +51,61 @@ impl<T> Drop for List<T> {
             cur_link = boxed_node.next.take();
         }
     }
+}
+
+pub struct IntoIter<T>(List<T>);
+
+impl<T> List<T> {
+    pub fn into_iter(self) -> IntoIter<T> {
+        IntoIter(self)
+    }
+}
+
+impl<T> Iterator for IntoIter<T> {
+    type Item = T;
+    fn next(&mut self) -> Option<Self::Item> {
+        // access fields of a tuple struct numerically
+        self.0.pop()
+    }
+}
+
+#[test]
+fn peek_test() {
+    let mut list = List::new();
+    assert_eq!(list.peek(), None);
+    assert_eq!(list.peek_mut(), None);
+    list.push(1); list.push(2); list.push(3);
+
+    assert_eq!(list.peek(), Some(&3));
+    assert_eq!(list.peek_mut(), Some(&mut 3));
+} 
+
+#[test]
+fn peek_mut_test() {
+    let mut list = List::new();
+    assert_eq!(list.peek(), None);
+    assert_eq!(list.peek_mut(), None);
+    list.push(1); list.push(2); list.push(3);
+
+    assert_eq!(list.peek(), Some(&3));
+    assert_eq!(list.peek_mut(), Some(&mut 3));
+
+    list.peek_mut().map(|value| {
+        *value = 42
+    });
+
+    assert_eq!(list.peek(), Some(&42));
+    assert_eq!(list.pop(), Some(42));
+}
+
+#[test]
+fn into_iter() {
+    let mut list = List::new();
+    list.push(1); list.push(2); list.push(3);
+
+    let mut iter = list.into_iter();
+    assert_eq!(iter.next(), Some(3));
+    assert_eq!(iter.next(), Some(2));
+    assert_eq!(iter.next(), Some(1));
+    assert_eq!(iter.next(), None);
 }
--- linked-lists-oh-boy/src/main.rs
+++ linked-lists-oh-boy/src/main.rs
@@ -8,5 +8,15 @@ fn main() {
     list.push(2);
     list.push(3);
     list.push(4);
-    list.pop();
+    
+    println!("* 4 == {:?}", list.peek());
+    println!("* 4 == {:?}", list.peek_mut());
+    println!("* 4 == {:?}", list.pop());
+    println!("* 3 == {:?}", list.peek());
+
+    println!("** Print Whole List ");
+    for e in list.into_iter() {
+        println!("{:?}", e);
+    }
+
 }
