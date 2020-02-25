980df2b0f8eeb4df6abba9058b453640db92700f
diegopacheco <diego.pacheco.it@gmail.com>
2020-02-25 19:35:47 -0300

    fun with mock_derive

---
+++ mock_derive_fun/src/main.rs
@@ -0,0 +1,46 @@
+#![feature(proc_macro)]
+extern crate mock_derive;
+
+use mock_derive::mock;
+
+pub fn main(){}
+
+pub struct Foo {}
+
+#[mock]
+pub trait CustomTrait {
+    fn new() -> Foo;
+    fn get_int(&self) -> u32;
+    fn opt_int(&self) -> Option<u32>;
+    fn default_method(&self, x: i32, y: i32) -> i32 {
+        x + y
+    }
+}
+
+impl CustomTrait for Foo {
+    fn new() -> Foo {
+        Foo { }
+    }
+    fn get_int(&self) -> u32 {
+        return 1;
+    }
+    fn opt_int(&self) -> Option<u32> {
+        return Some(1);
+    }
+}
+
+#[test]
+fn return_result_of() {
+    let mut x = 15;
+    let mut mock = MockCustomTrait::new();
+    let method = mock.method_opt_int()
+        .return_result_of(move || {
+            x += 1;
+            Some(x)
+        });
+
+    mock.set_opt_int(method);
+    assert!(mock.opt_int() == Some(16));
+    assert!(mock.opt_int() == Some(17));
+    assert!(mock.opt_int() == Some(18));
+}
