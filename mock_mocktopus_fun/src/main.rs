088b100d2ecc43886f4454b432358d12db81e71c
diegopacheco <diego.pacheco.it@gmail.com>
2020-02-25 20:10:34 -0300

    fun with mocktopus

--- mock_mocktopus_fun/src/main.rs
+++ mock_mocktopus_fun/src/main.rs
@@ -1,17 +1,15 @@
 use mocktopus_macros::mockable;
-use mocktopus::mocking::MockResult;
-use mocktopus::mocking::*;
 
 fn main() {
     println!("Hello, world!");
 }
 
 #[mockable]
+#[allow(dead_code)]
 mod hello_world {
     pub fn world() -> &'static str {
         "world"
     }
-
     pub fn hello_world() -> String {
         format!("Hello {}!", world())
     }
@@ -19,6 +17,9 @@ mod hello_world {
 
 #[test]
 fn mock_test() {
+    use mocktopus::mocking::MockResult;
+    use mocktopus::mocking::*;
+
     hello_world::world.mock_safe(|| MockResult::Return("mocking"));
     assert_eq!("Hello mocking!", hello_world::hello_world());
 }
