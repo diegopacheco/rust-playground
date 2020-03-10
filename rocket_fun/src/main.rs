715e767c7e8b4e602d64e9ff9b0b7a52dc125f41
diegopacheco <diego.pacheco.it@gmail.com>
2020-03-10 10:02:34 -0300

    fun with rocket

---
+++ rocket_fun/src/main.rs
@@ -0,0 +1,11 @@
+#![feature(proc_macro_hygiene, decl_macro)]
+#[macro_use] extern crate rocket;
+
+#[get("/<name>/<age>")]
+fn hello(name: String, age: u8) -> String {
+    format!("Hello, {} year old named {}!", age, name)
+}
+
+fn main() {
+    rocket::ignite().mount("/hello", routes![hello]).launch();
+}
