24307143c90faef36858afb912bc99020c9c1ddf
diegopacheco <diego.pacheco.it@gmail.com>
2020-03-10 20:54:08 -0300

    borrow and ownership sample

---
+++ borrow_and_ownership/src/main.rs
@@ -0,0 +1,30 @@
+fn main() {
+
+    moved_values_borrowing_to_fix();
+    moving();
+}
+
+fn moved_values_borrowing_to_fix(){
+    let s = String::from("Hello World");
+    // let z = s;
+    // borrow of moved value: `s`
+    let _z = &s;
+    println!("{:?}",s);
+}
+
+fn moving(){
+    let mut v = Vec::new();
+    for i in 1..100{
+        v.push(i);
+    }
+    v = moveme(v);
+    // moveme(v);
+    // borrow of moved value: `v`
+    println!("{:?}", v);
+}
+
+// fn moveme(v:Vec<i32>) ->  { .. } 
+fn moveme(v:Vec<i32>) -> Vec<i32> {
+    println!("{:?}", v);
+    return v;
+}
