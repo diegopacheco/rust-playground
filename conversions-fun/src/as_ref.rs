226a327a14366f6f8b7a975b89c6a911d05fdcbc
diegopacheco <diego.pacheco.it@gmail.com>
2020-03-23 13:31:46 -0300

    fun with conversions

---
+++ conversions-fun/src/as_ref.rs
@@ -0,0 +1,30 @@
+fn byte_counter<T: AsRef<str>>(arg: T) -> usize {
+    arg.as_ref().as_bytes().len()
+}
+
+fn char_counter<T: AsRef<str>>(arg: T) -> usize {
+    arg.as_ref().chars().count()
+}
+
+pub fn main_mod() {
+    let s = "Café au lait";
+    println!("{}", char_counter(s));
+    println!("{}", byte_counter(s));
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn different_counts() {
+        let s = "Café au lait";
+        assert_ne!(char_counter(s), byte_counter(s));
+    }
+
+    #[test]
+    fn same_counts() {
+        let s = "Cafe au lait";
+        assert_eq!(char_counter(s), byte_counter(s));
+    }
+}
---
+++ conversions-fun/src/main.rs
@@ -0,0 +1,6 @@
+mod as_ref;
+
+fn main() {
+    as_ref::main_mod();
+}
+
