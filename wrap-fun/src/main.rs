d850fac8852db57e12befd827a0caa1e509e954b
diegopacheco <diego.pacheco.it@gmail.com>
2020-04-26 02:04:27 -0300

    fun with wrap

---
+++ wrap-fun/src/main.rs
@@ -0,0 +1,11 @@
+use warp::Filter;
+
+#[tokio::main]
+async fn main() {
+    let hello = warp::path!("hello" / String)
+        .map(|name| format!("Hello, {}!", name));
+
+    warp::serve(hello)
+        .run(([127, 0, 0, 1], 3030))
+        .await;
+}
