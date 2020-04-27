9b9baac5f040a1c4e2e0c2963f7566f585fdea64
diegopacheco <diego.pacheco.it@gmail.com>
2020-04-27 20:32:42 -0300

    some WAT with rust

---
+++ crazy-stuff/src/main.rs
@@ -0,0 +1,12 @@
+fn main(){
+    wat1();
+    wat2();
+}
+
+fn wat2() {
+    (|| || || || || || || || || ())()()()()()()()()()
+}
+
+fn wat1(){
+    return return return return return return return return return return return return
+}
