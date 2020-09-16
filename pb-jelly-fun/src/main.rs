57d30f90dc197bac4b9d991bc939fc905eba9c77
diegopacheco <diego.pacheco.it@gmail.com>
2020-09-16 14:39:34 -0300

    not working

---
+++ pb-jelly-fun/src/main.rs
@@ -0,0 +1,24 @@
+use pb_jelly::Message;
+use proto_user::user::HelloUser;
+
+fn main() -> std::io::Result<()> {
+    // Say hello!
+    let msg = HelloUser {
+        name: "Parker".to_string(),
+    };
+
+    // Serialize our message to bytes
+    let se_msg = msg.serialize_to_vec();
+    println!("Bytes 0x{:X?}", se_msg);
+
+    // De-serialize our message back to a Rust struct
+    let de_msg: HelloUser = Message::deserialize_from_slice(&se_msg[..])?;
+
+    // Pretty print!
+    println!("Message: {:#?}", de_msg);
+
+    // Assert our two messages are the same
+    assert_eq!(msg, de_msg);
+
+    Ok(())
+}
