4697c1cbd640f5a1f83c0526d87581626afb4f29
diegopacheco <diego.pacheco.it@gmail.com>
2020-12-23 15:59:34 -0300

    box and rc

--- box-rc-fun/src/main.rs
+++ box-rc-fun/src/main.rs
@@ -2,13 +2,14 @@ use std::rc::Rc;
 
 fn main() {
 
-    // used once - read and write.
+    // used once - read and write. Exclusive Ownership
     let mut data = Box::new("data");
-    println!("{:#?}",&data);
-    data = Box::new("new Data");
-    println!("{:#?}",&data);
+    println!("BOX {:#?}",&data);
+    *data = "new Data";
+    println!("BOX {:#?}",&data);
 
-    // read only for any number of users
+    // read only for any number of users - Shared Ownership
     let mut ref_count = Rc::new("data");
-    println!("{:#?}",&ref_count);
+    println!("RC {:#?}",&ref_count);
+    //*ref_count = "new Data"; // error - cannot assign
 }
