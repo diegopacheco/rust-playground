25da8fdae7ba89a0cade7053726b6ce64927bf6a
diegopacheco <diego.pacheco.it@gmail.com>
2021-01-12 21:47:05 -0300

    fun with arc and mutex

---
+++ Arc-fun/src/main.rs
@@ -0,0 +1,19 @@
+use std::sync::Arc;
+use std::thread;
+
+fn main() {
+    // This variable declaration is where it's value is specified.
+    let apple = Arc::new("the same apple");
+
+    for _ in 0..10 {
+        // Here there is no value specification as it is a pointer to a reference
+        // in the memory heap.
+        let apple = Arc::clone(&apple);
+
+        thread::spawn(move || {
+            // As Arc was used, threads can be spawned using the value allocated
+            // in the Arc variable pointer's location.
+            println!("{:?}", apple);
+        });
+    }
+}
