bb60d9800704104132a79a5649fbba3af4b25a28
diegopacheco <diego.pacheco.it@gmail.com>
2021-05-11 01:35:57 -0300

    arithimetic pointers in rust

--- pointer-arithmetic-fun/src/StdOffset.rs
+++ pointer-arithmetic-fun/src/StdOffset.rs
@@ -6,4 +6,9 @@ pub fn compute(){
     println!("should be 1 == {}", unsafe { *ptr.offset(-1) });
     println!("should be 3 == {}", unsafe { *ptr.offset(1) });
     println!("should be 4 == {}", unsafe { *ptr.offset(2) });
+
+    let items2 = [10, 20, 30, 40];
+    let ptr2 = &items2[0] as *const i32;
+    println!("should be 10 == {}", unsafe { *ptr2 });
+    println!("should be 40 == {}", unsafe { *ptr2.offset(3) });
 }
