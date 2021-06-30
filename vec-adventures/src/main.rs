6ac090fe9110b0b259b1fd69ee2da6e4d352b270
diegopacheco <diego.pacheco.it@gmail.com>
2021-06-30 13:21:09 -0300

    vec adventures in rust

--- vec-adventures/src/main.rs
+++ vec-adventures/src/main.rs
@@ -1,14 +1,12 @@
 use std::collections::VecDeque;
-use std::rc::Rc;
-use std::cell::Cell;
 
 fn main() {
 
     let r1:VecDeque<i32> = to_vecdeque(vec![1,2,3]);
     println!("vec to VecDeque {:?}",r1);
 
-    let r2:Rc<Cell<Vec<&str>>> = to_str_vec(vec!["a".to_string(), "b".to_string()]);
-    println!("vec<&str> to Vec<String> {:?}", r2.take());
+    let r2:Vec<&'static str> = to_str_vec(vec!["a".to_string(), "b".to_string()]);
+    println!("vec<&str> to Vec<String> {:?}", r2);
 
    // let r3:Vec<&String> = to_string_vec(&vec![&"a", &"b"]);
    // println!("vec<String> to Vec<&str> {:?}", r3);
@@ -18,22 +16,12 @@ fn to_vecdeque(vector:Vec<i32>) -> VecDeque<i32> {
     VecDeque::from(vector)
 }
 
-/*
 fn to_str_vec<'a>(v:Vec<String>) -> Vec<&'static str> {
     let mut items = Vec::<&str>::new();
     for item in v {
         items.push(&item.as_str().clone());
     }
     items
-}
-*/
-
-fn to_str_vec<'a>(v:Vec<String>) -> Rc<Cell<Vec<&'a str>>> {
-    let mut items = Vec::<&str>::new();
-    for item in v {
-        items.push(&item.as_str().clone() );
-    }
-    Rc::new(Cell::from(items))
 }
 
 /*
