62df6431a78324eaf5c4f8909a0864cc8b542c19
diegopacheco <diego.pacheco.it@gmail.com>
2021-06-30 04:21:08 -0300

    vec adventures in rust

--- vec-adventures/src/main.rs
+++ vec-adventures/src/main.rs
@@ -1,5 +1,6 @@
 use std::collections::VecDeque;
 use std::borrow::Borrow;
+use std::iter::FromIterator;
 
 fn main() {
     println!("vec to VecDeque {:?}",to_vecdeque(vec![1,2,3]));
@@ -10,10 +11,6 @@ fn to_vecdeque(vector:Vec<i32>) -> VecDeque<i32> {
     VecDeque::from(vector)
 }
 
-fn to_vec(v:Vec<String>) -> Vec<&'static str>{
-    let mut items = Vec::<&str>::with_capacity(v.len());
-    for item in v {
-        items.push(item.clone().as_str());
-    }
-    items
+fn to_vec(v:Vec<String>) -> Vec<&'static str> {
+    Vec::from_iter( v.iter().map(String::as_str))
 }
