4bfef709be09a81398edcca69d2d3639b0fb75ec
diegopacheco <diego.pacheco.it@gmail.com>
2021-07-02 02:18:23 -0300

    Rc and CellRef in Rust

--- rc-cell-fun/src/main.rs
+++ rc-cell-fun/src/main.rs
@@ -5,7 +5,7 @@ use std::cell::RefCell;
  * Rc: A single-threaded reference-counting pointer.
  * 'Rc' stands for 'Reference Counted'
  * RefCell: A mutable memory location with dynamically
- * checked borrow rule
+ *  checked borrow rule
  */
 fn main() {
     let answer = String::from("Gandalfy");
@@ -16,21 +16,16 @@ fn main() {
 
     print_rc_cell(Rc::clone(&shared_string));
     add_dot_toupper(Rc::clone(&shared_string));
-
     print_rc_cell(Rc::clone(&shared_string));
 
     println!("main - Owners == {}",Rc::strong_count(&shared_string));
-    drop(&shared_string);
+
+    drop(shared_string);
 }
 
 fn add_dot_toupper(s:Rc<RefCell<String>>){
     println!("add_dot_toupper - Owners == {}",Rc::strong_count(&s));
     s.replace(s.take().to_uppercase() + ".");
-}
-
-fn add_stars(s:Rc<RefCell<String>>){
-    println!("add_stars - Owners == {}",Rc::strong_count(&s));
-    s.replace(s.take().to_uppercase() + "***");
 }
 
 fn print_rc_cell(s:Rc<RefCell<String>>){
