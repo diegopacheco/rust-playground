7fb5d46961f031025668b8f6023a91832844f99d
diegopacheco <diego.pacheco.it@gmail.com>
2022-12-13 00:41:31 -0800

    tauri

---
+++ tauri-simple-fun/tauri-app/src/main.js
@@ -0,0 +1,17 @@
+const { invoke } = window.__TAURI__.tauri;
+
+let greetInputEl;
+let greetMsgEl;
+
+async function greet() {
+  // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
+  greetMsgEl.textContent = await invoke("greet", { name: greetInputEl.value });
+}
+
+window.addEventListener("DOMContentLoaded", () => {
+  greetInputEl = document.querySelector("#greet-input");
+  greetMsgEl = document.querySelector("#greet-msg");
+  document
+    .querySelector("#greet-button")
+    .addEventListener("click", () => greet());
+});
