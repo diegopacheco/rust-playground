0c01f198ce42ab460f5dfcd1518294ae53ab39d3
diegopacheco <diego.pacheco.it@gmail.com>
2023-09-04 21:06:16 -0700

    axum in rust

---
+++ axum-fun/axum-fun/src/main.rs
@@ -0,0 +1,16 @@
+use axum::{
+    routing::get,
+    Router,
+};
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/", get(|| async { "Rust / AXUM : Hello, World!" }));
+
+    println!("Axum up and running on http://127.0.0.1:3000");
+
+    axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+}
