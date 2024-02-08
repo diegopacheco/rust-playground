17fc8a470e58c2e209507cf1b96fd7305e3fe243
diegopacheco <diego.pacheco.it@gmail.com>
2024-02-08 00:24:17 -0800

    rustpython - running python on rust

---
+++ rust-python-fun/README.md
@@ -0,0 +1,27 @@
+### Install RustPython
+```bash
+cargo install --git https://github.com/RustPython/RustPython
+```
+
+### Build
+```bash
+cargo build
+```
+
+### Run
+```bash
+cargo run
+```
+
+### Python REPL
+```bash
+```
+rustpython
+```
+❯ rustpython
+Welcome to the magnificent Rust Python 0.3.0 interpreter 😱 🖖
+No previous history.
+RustPython 3.12.0
+Type "help", "copyright", "credits" or "license" for more information.
+>>>>>
+```
---
+++ rust-python-fun/src/main.rs
@@ -0,0 +1,14 @@
+use rustpython_vm as vm;
+
+fn main() -> vm::PyResult<()> {
+    vm::Interpreter::without_stdlib(Default::default()).enter(|vm| {
+        let scope = vm.new_scope_with_builtins();
+        let source = r#"print("Hello World!")"#;
+        let code_obj = vm
+            .compile(source, vm::compiler::Mode::Exec, "<embedded>".to_owned())
+            .map_err(|err| vm.new_syntax_error(&err, Some(source)))?;
+
+        vm.run_code_obj(code_obj, scope)?;
+        Ok(())
+    })
+}
