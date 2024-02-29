f03b7d22060180c6f431b094664a05b592873ec4
diegopacheco <diego.pacheco.it@gmail.com>
2024-02-29 01:16:02 -0800

    rust poc - try / from

---
+++ try-from-trait-fun/src/main.rs
@@ -0,0 +1,30 @@
+use std::convert::TryFrom;
+use std::convert::TryInto;
+
+#[derive(Debug, PartialEq)]
+struct EvenNumber(i32);
+
+impl TryFrom<i32> for EvenNumber {
+    type Error = ();
+    fn try_from(value: i32) -> Result<Self, Self::Error> {
+        if value % 2 == 0 {
+            Ok(EvenNumber(value))
+        } else {
+            Err(())
+        }
+    }
+}
+
+fn main() {
+    // TryFrom
+    assert_eq!(EvenNumber::try_from(8), Ok(EvenNumber(8)));
+    assert_eq!(EvenNumber::try_from(5), Err(()));
+    println!("{:?}",EvenNumber::try_from(8));
+
+    // TryInto
+    let result: Result<EvenNumber, ()> = 8i32.try_into();
+    assert_eq!(result, Ok(EvenNumber(8)));
+    let result: Result<EvenNumber, ()> = 5i32.try_into();
+    assert_eq!(result, Err(()));
+    println!("{:?}",<i32 as TryInto<i32>>::try_into(8i32));
+}
