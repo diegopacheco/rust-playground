9ca7bdb41b4ca4787a4f801b88cc4abe05a5ffc2
diegopacheco <diego.pacheco.it@gmail.com>
2024-05-06 01:25:18 -0700

    rust monoio

---
+++ monoio-fun/README.md
@@ -0,0 +1,22 @@
+### Build
+```bash
+cargo build
+```
+### Run
+```bash
+cargo run
+```
+
+### Result
+```
+cargo run
+```
+
+In a second terminal
+```
+❯ ./test.sh
+1234
+1234
+echo
+echo
+```
---
+++ monoio-fun/src/main.rs
@@ -0,0 +1,38 @@
+use monoio::io::{AsyncReadRent, AsyncWriteRentExt};
+use monoio::net::{TcpListener, TcpStream};
+
+#[monoio::main]
+async fn main() {
+    let listener = TcpListener::bind("127.0.0.1:50002").unwrap();
+    println!("listening");
+    loop {
+        let incoming = listener.accept().await;
+        match incoming {
+            Ok((stream, addr)) => {
+                println!("accepted a connection from {}", addr);
+                monoio::spawn(echo(stream));
+            }
+            Err(e) => {
+                println!("accepted connection failed: {}", e);
+                return;
+            }
+        }
+    }
+}
+
+async fn echo(mut stream: TcpStream) -> std::io::Result<()> {
+    let mut buf: Vec<u8> = Vec::with_capacity(8 * 1024);
+    let mut res;
+    loop {
+        // read
+        (res, buf) = stream.read(buf).await;
+        if res? == 0 {
+            return Ok(());
+        }
+        // write all
+        (res, buf) = stream.write_all(buf).await;
+        res?;
+        // clear
+        buf.clear();
+    }
+}
