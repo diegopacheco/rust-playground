2b858e008c78c2ba1ed0627c9eeb87f2d94f1df7
diegopacheco <diego.pacheco.it@gmail.com>
2024-11-03 21:26:19 -0800

    ractor

--- ractor-fun/src/main.rs
+++ ractor-fun/src/main.rs
@@ -1,24 +1,42 @@
-use async_trait::async_trait;
-use ractor::{Actor, ActorRef, ActorProcessingErr};
+use ractor::{Actor, ActorRef, ActorProcessingErr, RpcReplyPort};
+
+pub enum MyFirstActorMessage {
+    /// Print's hello world
+    PrintHelloWorld,
+    /// Replies with how many hello worlds have occurred
+    HowManyHelloWorlds(RpcReplyPort<u16>),
+}
 
 pub struct MyFirstActor;
 
 #[async_trait::async_trait]
 impl Actor for MyFirstActor {
-    type State = ();
+    type State = u16;
     type Msg = MyFirstActorMessage;
     type Arguments = ();
 
     async fn pre_start(&self, _myself: ActorRef<Self::Msg>, _arguments: Self::Arguments)
-        -> Result<Self::State, ActorProcessingErr> 
+        -> Result<Self::State, ActorProcessingErr>
     {
+        Ok(0)
+    }
+
+    async fn handle(&self, _myself: ActorRef<Self::Msg>, message: Self::Msg, state: &mut Self::State) 
+        -> Result<(), ActorProcessingErr>
+    {
+        match message {
+            MyFirstActorMessage::PrintHelloWorld => {
+                println!("Hello world!");
+                *state += 1;
+            }
+            MyFirstActorMessage::HowManyHelloWorlds(reply) => {
+                if reply.send(*state).is_err() {
+                    println!("Listener dropped their port before we could reply");
+                }
+            }
+        }
         Ok(())
     }
-}
-
-pub enum MyFirstActorMessage {
-    /// Print's hello world
-    PrintHelloWorld,
 }
 
 #[tokio::main]
@@ -27,15 +45,22 @@ async fn main() {
     // actor. Most of the time we drop this handle, but it's handy in the 
     // main function to wait for clean actor shut-downs (all stop handlers will
     // have completed)
-    let (actor, actor_handle) = Actor::spawn(None, MyFirstActor, ()).await.expect("Actor failed to start");
+    let (actor, actor_handle) = 
+        Actor::spawn(None, MyFirstActor, ())
+            .await
+            .expect("Actor failed to start");
     
     for _i in 0..10 {
         // Sends a message, with no reply
-        actor.cast(MyFirstActorMessage::PrintHelloWorld).expect("Failed to send message to actor");
+        actor.cast(MyFirstActorMessage::PrintHelloWorld)
+            .expect("Failed to send message to actor");
     }
 
-    // give a little time to print out all the messages
-    tokio::time::sleep(tokio::time::Duration::from_millis(100)).await;
+    let hello_world_count = 
+        ractor::call_t!(actor, MyFirstActorMessage::HowManyHelloWorlds, 100)
+        .expect("RPC failed");
+    
+    println!("Actor replied with {} hello worlds!", hello_world_count);
 
     // Cleanup
     actor.stop(None);
