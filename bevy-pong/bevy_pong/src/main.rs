71101fd652731d2978a352831c11859b6f317000
diegopacheco <diego.pacheco.it@gmail.com>
2025-05-18 23:13:08 -0700

    bevy/pong

--- bevy-pong/bevy_pong/src/main.rs
+++ bevy-pong/bevy_pong/src/main.rs
@@ -3,7 +3,6 @@ use bevy::{
     prelude::*,
     sprite::MaterialMesh2dBundle,
     window::{PresentMode, WindowResolution},
-    asset::AssetMode,
 };
 use rand::Rng;
 
@@ -44,26 +43,17 @@ struct Score {
 
 fn main() {
     App::new()
-        .insert_resource(ClearColor(Color::rgb(0.0, 0.0, 0.1)))
+        .insert_resource(ClearColor(Color::rgb(0.0, 0.0, 0.1))) // Dark blue background
         .insert_resource(Score { left: 0, right: 0 })
-        .add_plugins(
-            DefaultPlugins
-                .set(AssetPlugin {
-                    file_path: "assets".to_string(),
-                    processed_file_path: "assets_processed".to_string(),
-                    watch_for_changes_override: Some(true),
-                    mode: AssetMode::Unprocessed,
-                })
-                .set(WindowPlugin {
-                    primary_window: Some(Window {
-                        title: "Bevy Pong".into(),
-                        resolution: WindowResolution::new(WINDOW_WIDTH, WINDOW_HEIGHT),
-                        present_mode: PresentMode::AutoVsync,
-                        ..default()
-                    }),
-                    ..default()
-                }),
-        )
+        .add_plugins(DefaultPlugins.set(WindowPlugin {
+            primary_window: Some(Window {
+                title: "Bevy Pong".into(),
+                resolution: WindowResolution::new(WINDOW_WIDTH, WINDOW_HEIGHT),
+                present_mode: PresentMode::AutoVsync,
+                ..default()
+            }),
+            ..default()
+        }))
         .add_systems(Startup, setup)
         .add_systems(
             Update,
@@ -82,7 +72,6 @@ fn setup(
     mut commands: Commands,
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<ColorMaterial>>,
-    asset_server: Res<AssetServer>,
 ) {
     // Camera
     commands.spawn(Camera2dBundle::default());
@@ -122,8 +111,29 @@ fn setup(
         Collider,
     ));
 
-    // Ball
-    spawn_ball(&mut commands, &mut materials, &mut meshes);
+    // Ball - Make it bright yellow for visibility
+    commands.spawn((
+        MaterialMesh2dBundle {
+            mesh: meshes.add(Rectangle::new(BALL_SIZE, BALL_SIZE)).into(),
+            material: materials.add(Color::rgb(1.0, 1.0, 0.0)), // Bright yellow
+            transform: Transform::from_translation(Vec3::new(0.0, 0.0, 0.0)),
+            ..default()
+        },
+        Ball {
+            velocity: Vec2::new(
+                if rand::thread_rng().gen_bool(0.5) {
+                    BALL_SPEED
+                } else {
+                    -BALL_SPEED
+                },
+                if rand::thread_rng().gen_bool(0.5) {
+                    BALL_SPEED * 0.5
+                } else {
+                    -BALL_SPEED * 0.5
+                },
+            ),
+        },
+    ));
 
     // Walls
     // Top wall
@@ -173,44 +183,38 @@ fn setup(
         ..default()
     });
 
-    // Score text
-    let font = asset_server.load("fonts/FiraSans-Bold.ttf");
-
+    // Score text - Use Text2dBundle instead of TextBundle for better positioning in 2D space
     // Left score
     commands.spawn((
-        TextBundle::from_sections([TextSection::new(
-            "0",
-            TextStyle {
-                font: font.clone(),
-                font_size: 40.0,
-                color: Color::WHITE,
-            },
-        )])
-        .with_style(Style {
-            position_type: PositionType::Absolute,
-            left: Val::Px(WINDOW_WIDTH / 4.0),
-            top: Val::Px(20.0),
+        Text2dBundle {
+            text: Text::from_section(
+                "0",
+                TextStyle {
+                    font_size: 60.0,
+                    color: Color::WHITE,
+                    ..default()
+                },
+            ),
+            transform: Transform::from_translation(Vec3::new(-WINDOW_WIDTH / 4.0, WINDOW_HEIGHT / 3.0, 0.0)),
             ..default()
-        }),
+        },
         ScoreText { is_left: true },
     ));
 
     // Right score
     commands.spawn((
-        TextBundle::from_sections([TextSection::new(
-            "0",
-            TextStyle {
-                font,
-                font_size: 40.0,
-                color: Color::WHITE,
-            },
-        )])
-        .with_style(Style {
-            position_type: PositionType::Absolute,
-            left: Val::Px(3.0 * WINDOW_WIDTH / 4.0),
-            top: Val::Px(20.0),
+        Text2dBundle {
+            text: Text::from_section(
+                "0",
+                TextStyle {
+                    font_size: 60.0,
+                    color: Color::WHITE,
+                    ..default()
+                },
+            ),
+            transform: Transform::from_translation(Vec3::new(WINDOW_WIDTH / 4.0, WINDOW_HEIGHT / 3.0, 0.0)),
             ..default()
-        }),
+        },
         ScoreText { is_left: false },
     ));
 }
@@ -253,15 +257,6 @@ fn ball_movement(time: Res<Time>, mut query: Query<(&mut Transform, &Ball)>) {
     }
 }
 
-fn exit_system(
-    keyboard_input: Res<ButtonInput<KeyCode>>,
-    mut app_exit_events: EventWriter<AppExit>,
-) {
-    if keyboard_input.just_pressed(KeyCode::Escape) {
-        app_exit_events.send(AppExit);
-    }
-}
-
 fn check_collisions(
     mut commands: Commands,
     mut ball_query: Query<(Entity, &mut Ball, &Transform)>,
@@ -271,79 +266,114 @@ fn check_collisions(
     mut materials: ResMut<Assets<ColorMaterial>>,
 ) {
     if ball_query.is_empty() {
-        info!("No ball found in the world, spawning a new one");
-        spawn_ball(&mut commands, &mut materials, &mut meshes);
         return;
     }
 
-    for (ball_entity, mut ball, ball_transform) in ball_query.iter_mut() {
-        let ball_pos = ball_transform.translation;
+    let (ball_entity, mut ball, ball_transform) = ball_query.single_mut();
+    let ball_pos = ball_transform.translation;
 
-        // Check if ball is outside the screen (someone scored)
-        if ball_pos.x < -WINDOW_WIDTH / 2.0 {
-            // Right player scored
-            score.right += 1;
-            // Reset ball
-            commands.entity(ball_entity).despawn();
-            spawn_ball(&mut commands, &mut materials, &mut meshes);
-            return;
-        } else if ball_pos.x > WINDOW_WIDTH / 2.0 {
-            // Left player scored
-            score.left += 1;
-            // Reset ball
-            commands.entity(ball_entity).despawn();
-            spawn_ball(&mut commands, &mut materials, &mut meshes);
-            return;
-        }
+    // Check if ball is outside the screen (someone scored)
+    if ball_pos.x < -WINDOW_WIDTH / 2.0 {
+        // Right player scored
+        score.right += 1;
+        info!("Right player scored! Score: {}-{}", score.left, score.right);
+        
+        // Reset ball position and give it a new velocity
+        commands.entity(ball_entity).despawn();
+        
+        // Spawn a new ball
+        commands.spawn((
+            MaterialMesh2dBundle {
+                mesh: meshes.add(Rectangle::new(BALL_SIZE, BALL_SIZE)).into(),
+                material: materials.add(Color::rgb(1.0, 1.0, 0.0)), // Bright yellow
+                transform: Transform::from_translation(Vec3::new(0.0, 0.0, 0.0)),
+                ..default()
+            },
+            Ball {
+                velocity: Vec2::new(
+                    BALL_SPEED, // Always start towards who just scored
+                    if rand::thread_rng().gen_bool(0.5) {
+                        BALL_SPEED * 0.5
+                    } else {
+                        -BALL_SPEED * 0.5
+                    },
+                ),
+            },
+        ));
+    } else if ball_pos.x > WINDOW_WIDTH / 2.0 {
+        // Left player scored
+        score.left += 1;
+        info!("Left player scored! Score: {}-{}", score.left, score.right);
+        
+        // Reset ball position and give it a new velocity
+        commands.entity(ball_entity).despawn();
+        
+        // Spawn a new ball
+        commands.spawn((
+            MaterialMesh2dBundle {
+                mesh: meshes.add(Rectangle::new(BALL_SIZE, BALL_SIZE)).into(),
+                material: materials.add(Color::rgb(1.0, 1.0, 0.0)), // Bright yellow
+                transform: Transform::from_translation(Vec3::new(0.0, 0.0, 0.0)),
+                ..default()
+            },
+            Ball {
+                velocity: Vec2::new(
+                    -BALL_SPEED, // Always start towards who just scored
+                    if rand::thread_rng().gen_bool(0.5) {
+                        BALL_SPEED * 0.5
+                    } else {
+                        -BALL_SPEED * 0.5
+                    },
+                ),
+            },
+        ));
+    }
 
-        // Check collision with paddles and walls
-        for (collider_transform, maybe_paddle) in collider_query.iter() {
-            let collider_pos = collider_transform.translation;
+    // Check collision with paddles and walls
+    for (collider_transform, maybe_paddle) in collider_query.iter() {
+        let collider_pos = collider_transform.translation;
 
-            // Wall collision (top/bottom)
-            if maybe_paddle.is_none() {
-                let collision = collides_with_wall(
-                    ball_pos.x,
-                    ball_pos.y,
-                    BALL_SIZE / 2.0,
-                    collider_pos.x,
-                    collider_pos.y,
-                    WINDOW_WIDTH / 2.0,
-                    WALL_THICKNESS / 2.0,
-                );
+        // Wall collision (top/bottom)
+        if maybe_paddle.is_none() {
+            let collision = collides_with_wall(
+                ball_pos.x,
+                ball_pos.y,
+                BALL_SIZE / 2.0,
+                collider_pos.x,
+                collider_pos.y,
+                WINDOW_WIDTH / 2.0,
+                WALL_THICKNESS / 2.0,
+            );
 
-                if collision {
-                    ball.velocity.y = -ball.velocity.y;
-                }
+            if collision {
+                ball.velocity.y = -ball.velocity.y;
             }
-            // Paddle collision
-            else {
-                let paddle = maybe_paddle.unwrap();
+        }
+        // Paddle collision
+        else if let Some(paddle) = maybe_paddle {
+            let collision = collides_with_paddle(
+                ball_pos.x,
+                ball_pos.y,
+                BALL_SIZE / 2.0,
+                collider_pos.x,
+                collider_pos.y,
+                PADDLE_WIDTH / 2.0,
+                PADDLE_HEIGHT / 2.0,
+            );
 
-                let collision = collides_with_paddle(
-                    ball_pos.x,
-                    ball_pos.y,
-                    BALL_SIZE / 2.0,
-                    collider_pos.x,
-                    collider_pos.y,
-                    PADDLE_WIDTH / 2.0,
-                    PADDLE_HEIGHT / 2.0,
-                );
+            if collision {
+                // Bounce based on where the ball hit the paddle
+                let hit_pos = (ball_pos.y - collider_pos.y) / (PADDLE_HEIGHT / 2.0);
+                let bounce_angle = hit_pos * std::f32::consts::PI / 4.0; // Max 45 degree angle
 
-                if collision {
-                    // Bounce based on where the ball hit the paddle
-                    let hit_pos = (ball_pos.y - collider_pos.y) / (PADDLE_HEIGHT / 2.0);
-                    let bounce_angle = hit_pos * std::f32::consts::PI / 4.0; // Max 45 degree angle
+                let speed = ball.velocity.length();
+                let direction = if paddle.is_left { 1.0 } else { -1.0 };
 
-                    let speed = ball.velocity.length();
-                    let direction = if paddle.is_left { 1.0 } else { -1.0 };
+                ball.velocity.x = direction * bounce_angle.cos() * speed;
+                ball.velocity.y = bounce_angle.sin() * speed;
 
-                    ball.velocity.x = direction * bounce_angle.cos() * speed;
-                    ball.velocity.y = bounce_angle.sin() * speed;
-
-                    // Slightly increase speed after paddle hit
-                    ball.velocity *= 1.05;
-                }
+                // Slightly increase speed after paddle hit
+                ball.velocity *= 1.05;
             }
         }
     }
@@ -358,6 +388,16 @@ fn update_score_text(score: Res<Score>, mut query: Query<(&mut Text, &ScoreText)
                 score.right.to_string()
             };
         }
+    }
+}
+
+fn exit_system(
+    keyboard_input: Res<ButtonInput<KeyCode>>,
+    mut app_exit_events: EventWriter<AppExit>,
+) {
+    if keyboard_input.just_pressed(KeyCode::Escape) {
+        info!("Escape key pressed, exiting game");
+        app_exit_events.send(AppExit);
     }
 }
 
@@ -407,37 +447,4 @@ fn collides_with_paddle(
     let distance_y = ball_y - closest_y;
 
     distance_x * distance_x + distance_y * distance_y < ball_radius * ball_radius
-}
-
-// Function to spawn a new ball
-fn spawn_ball(
-    commands: &mut Commands,
-    materials: &mut ResMut<Assets<ColorMaterial>>,
-    meshes: &mut ResMut<Assets<Mesh>>,
-) {
-    let mut rng = rand::thread_rng();
-
-    // Use a bright yellow color for better visibility
-    commands.spawn((
-        MaterialMesh2dBundle {
-            mesh: meshes.add(Rectangle::new(BALL_SIZE, BALL_SIZE)).into(),
-            material: materials.add(Color::rgb(1.0, 1.0, 0.0)), // Bright yellow
-            transform: Transform::from_translation(Vec3::new(0.0, 0.0, 0.0)),
-            ..default()
-        },
-        Ball {
-            velocity: Vec2::new(
-                if rng.gen_bool(0.5) {
-                    BALL_SPEED
-                } else {
-                    -BALL_SPEED
-                },
-                if rng.gen_bool(0.5) {
-                    BALL_SPEED * 0.5
-                } else {
-                    -BALL_SPEED * 0.5
-                },
-            ),
-        },
-    ));
 }
