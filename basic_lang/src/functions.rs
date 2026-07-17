24ff7f9390e8abfbc2fd1ceeb935fb6de8d2fad5
diegopacheco <diego.pacheco.it@gmail.com>
2026-07-16 23:10:30 -0700

    added .

--- basic_lang/src/functions.rs
+++ basic_lang/src/functions.rs
@@ -1,10 +1,10 @@
-pub fn add_one(i : i32) -> i32{
+pub fn add_one(i: i32) -> i32 {
     i + 1
 }
 
-pub fn execute(){
+pub fn execute() {
     let i = add_one(10);
-    println!("addOne: {:?}",i);
-    let f:fn(i32)->i32 = add_one;
-    println!("addOne: {:?}",f(i));
+    println!("addOne: {:?}", i);
+    let f: fn(i32) -> i32 = add_one;
+    println!("addOne: {:?}", f(i));
 }
